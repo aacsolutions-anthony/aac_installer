84e224e40ec1e9a5399e9e906082c0fe5f9a0c09
aacsolutions-anthony <131961269+aacsolutions-anthony@users.noreply.github.com>
2023-08-23 16:02:09 +1000

    Add files via upload

--- AAC_Installer_Master.go
+++ AAC_Installer_Master.go
@@ -1,61 +1,156 @@
+package main
+
+/*
+
+
+ADDITIONS:
+
+	1>Переместить папку резервных копий SQL
+	2>Проверить извлечение содержимого
+	3>Упаковать внутренний файл в 'Advanced Installer'
+	4>Получить тихие установки и запустить их внутри новой функции
+
+MOVE SQL BACKUPS FOLDER
+CHECK EXTRACT CONTENTS
+PACKAGE INSIDE ADVANCED INSTALLER
+GET SILENT INSTALLS AND RUN THEM INSIDE A NEW FUNCTION
+func silentInstall (){
+	postman
+	erlang
+	notepad ++
+	C/C++ RUNTIME
+
+}
+
+Other installs such as Microsoft SQL and studio must be done manually.
+func sqlServer BAK/SQLI (){
+	chk// call after SQLServer() main install
+	chk// call after SQL backups folder has moved to C:/X
+
+
+
+}
+
+func mainInstalls (){
+	SQLServer()
+
+
+}
+
+REFACTOR TO USE LESS NEWLINES AND PROCESS STARTS, POSSIBLE MULTITHREAD (MAX 4 THREADS / DAEMONS / DAEMONS NEED EXTRA CHECKS) UPDATE ON V2.0.0
+
+
+
+GO LANG
+AAC INSTALLER SOURCE CODE
+TO BE PACKAGED INSIDE OF ADVANCED INSTALLER
+         ____
+        / _\ \
+      .`\/  \ \
+    ,`   \   \ \
+     / /-`    \ \ .
+    / /       ,\ `|
+   / /        `-._|
+  / /_.`|________\_\
+  \/_<  ___________/
+GO-   `.|
+
+
 // The author of this code is not affiliated with, endorsed by, or associated with Microsoft or any of its subsidiaries or affiliates.
 //
 //DATE          : 23/08/2023
 //AUTHOR        : ANTHONY GRACE
 //COMPANY       : AAC SOLUTIONS PTY LTD
 //DEPARTMENT    : IT TECHNICIAN
-//COMP / VERSION: BETA-0.0.1
+//COMP / VERSION: BETA-0.0.2
 //
-// PRG: AAC INSTALLER 
-// PUR: Install and automate the server setup checklist. 
+// PRG: AAC INSTALLER
+// PUR: Install and automate the server setup checklist.
 //
 
-
-import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
-	"os/exec"
-	"strings"
-)
-
-
-
-//IMPORT OTHER VARIABLES HERE
-// JSON VARS TO COME FROM MATT
-
-type Config struct {
-	AuthToken string `json:"authtoken"`
-	Label     string `json:"label"`
-	Protocol  string `json:"protocol"`
-	Port      int    `json:"port"`
-}
-
-func buildPowerShellScript(lines ...string) string {
-	return strings.Join(lines, "; ")
-}
-
-
-
-// BELOW TO BE IMPLEMENTED, SSL CERT GENERATION
-
-package main
-
+*/
 import (
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"crypto/x509/pkix"
-	"io/ioutil"
+	"encoding/json"
+	"fmt"
 	"log"
 	"math/big"
+	"os"
+	"os/exec"
+	"strings"
 	"time"
 )
 
-var pem = "cert.pem"
+func pullconfig() Config {
+	configFile := "goconfig.json"
+	var config Config
+	file, err := os.ReadFile(configFile)
+	if err != nil {
+		log.Fatalf("Failed to read config file %s: %v", configFile, err)
+	}
+	if err := json.Unmarshal(file, &config); err != nil {
+		log.Fatalf("Failed to unmarshal JSON: %v", err)
+	}
+	return config
+}
+
+type Config struct {
+	AuthToken   string `json:"authtoken"`
+	Label       string `json:"label"`
+	Protocol    string `json:"protocol"`
+	Port        int    `json:"port"`
+	RegistryKey string `json:"registryKey"`
+}
+
+var csr = "cert.csr"
 var key = "cert.key"
+
+func buildPowerShellScript(lines ...string) string {
+	return strings.Join(lines, "; ")
+}
+
+func runPowerShellCommand(command string) {
+	cmd := exec.Command("powershell", "-command", command)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatalf("Failed to execute command: %s\nError: %v\n%s\n", command, err, out)
+	}
+}
+
+func powershellList(config Config) {
+
+	fmt.Println("COPYING PULSE LIVE PATCH: ")
+	runPowerShellCommand(`Copy-Item -Path .\PATCH\Server\* -Destination 'C:\program files\pulselive' -Recurse -Force`)
+	runPowerShellCommand(`Copy-Item -Path .\PATCH\Max\* -Destination 'C:\program files\pulselive\max' -Recurse -Force`)
+	runPowerShellCommand(`Expand-Archive -Path .\PATCH\Client.zip -DestinationPath 'c:\inetpub\wwwroot\pulselive' -Force`)
+	time.Sleep(1 * time.Second)
+
+	fmt.Println("PERFORMING IIS CONFIGURATION: ")
+	runPowerShellCommand(`Import-Module WebAdministration`)
+	runPowerShellCommand(`Set-ItemProperty -Path 'IIS:\AppPools\DefaultAppPool' -Name Recycling.periodicRestart.schedule -Value @{value='06:00:00','09:00:00'}`)
+	runPowerShellCommand(`New-WebApplication -Name "PulseLive" -Site "Default Web Site" -PhysicalPath 'c:\inetpub\wwwroot\pulselive' -ApplicationPool "DefaultAppPool"`)
+	time.Sleep(1 * time.Second)
+
+	fmt.Println("CONFIGURING REGISTRY: ")
+	runPowerShellCommand(`New-Item -Path 'HKLM:\Software\AAC\L' -Force`)
+	runPowerShellCommand(fmt.Sprintf(`New-ItemProperty -Path 'HKLM:\Software\AAC\L' -Name 'K' -Value '%s' -PropertyType 'String' -Force`, config.RegistryKey))
+	time.Sleep(1 * time.Second)
+
+	fmt.Println("APPLYING PULSELIVE LICENSE: ")
+	runPowerShellCommand(`Copy-Item -Path .\pulselive.lic -Destination 'c:\program files\pulselive' -Force`)
+	time.Sleep(1 * time.Second)
+
+	fmt.Println("ADJUSTING POWER SETTINGS: ")
+	runPowerShellCommand(`powercfg /change -standby-timeout-ac 0`)
+	time.Sleep(1 * time.Second)
+
+	fmt.Println("CHANGING FOLDER PERMS: ")
+	runPowerShellCommand(`$acl = Get-Acl 'c:\inetpub\wwwroot\pulselive\CreatedReports'; $permission = 'IIS_IUSRS','FullControl','Allow'; $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule $permission; $acl.SetAccessRule($accessRule); $acl | Set-Acl 'c:\inetpub\wwwroot\pulselive\CreatedReports'`)
+	runPowerShellCommand(`$acl = Get-Acl 'c:\inetpub\wwwroot\pulselive\Logs'; $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule $permission; $acl.SetAccessRule($accessRule); $acl | Set-Acl 'c:\inetpub\wwwroot\pulselive\Logs'`)
+}
 
 func genCert() {
 	ca := &x509.Certificate{
@@ -81,19 +176,113 @@ func genCert() {
 	ca_b, err := x509.CreateCertificate(rand.Reader, ca, ca, pub, priv)
 	if err != nil {
 		log.Fatalf("create cert failed %#v", err)
-		return
 	}
-	log.Println("save", pem)
-	ioutil.WriteFile(pem, ca_b, 0644)
+	log.Println("save", csr)
+	os.WriteFile(csr, ca_b, 0644)
 	log.Println("save", key)
-	ioutil.WriteFile(key, x509.MarshalPKCS1PrivateKey(priv), 0644)
+	os.WriteFile(key, x509.MarshalPKCS1PrivateKey(priv), 0644)
+}
+
+func InstallAndConfigureNgrok(config Config) {
+
+	fmt.Printf("Welcome to the AAC Solutions installer to install and configure ngrok software.\n")
+	time.Sleep(1 * time.Second)
+
+	fmt.Printf("Current authtoken: %s\n", config.AuthToken)
+	time.Sleep(1 * time.Second)
+
+	fmt.Printf("Current label: %s\n", config.Label)
+	time.Sleep(1 * time.Second)
+
+	fmt.Printf("Current protocol: %s\n", config.Protocol)
+	time.Sleep(1 * time.Second)
+
+	fmt.Printf("Current port: %d\n", config.Port)
+	time.Sleep(1 * time.Second)
+
+	fmt.Print("Would you like to change these values? (yes/no): ")
+	time.Sleep(1 * time.Second)
+
+	var response string
+	fmt.Scanln(&response)
+	response = strings.ToLower(response)
+
+	validResponses := []string{"yes", "y"}
+	for _, validResponse := range validResponses {
+		if response == validResponse {
+			fmt.Print("Enter new authtoken: ")
+			fmt.Scanln(&config.AuthToken)
+			fmt.Print("Enter new label: ")
+			fmt.Scanln(&config.Label)
+			fmt.Print("Enter new protocol: ")
+			fmt.Scanln(&config.Protocol)
+			fmt.Print("Enter new port: ")
+			fmt.Scanln(&config.Port)
+			break
+		}
+	}
+
+	ngrokPath := ".\\ngrok\\ngrok.exe"
+	if _, err := os.Stat(ngrokPath); os.IsNotExist(err) {
+		installScript := buildPowerShellScript(
+			`$url = "https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-windows-amd64.zip"; $output = "ngrok-v3-stable-windows-amd64.zip"`,
+			"Invoke-WebRequest -Uri $url -OutFile $output",
+			"Expand-Archive -Path $output -DestinationPath .\\ngrok",
+			"Set-Location -Path .\\ngrok",
+		)
+		cmd := exec.Command("powershell", "-nologo", "-noprofile", "-command", installScript)
+		_, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	runScript := buildPowerShellScript(
+		fmt.Sprintf(".\\ngrok.exe config add-authtoken %s", config.AuthToken),
+		fmt.Sprintf(".\\ngrok.exe tunnel --label %s %s://localhost:%d", config.Label, config.Protocol, config.Port),
+	)
+
+	cmd := exec.Command("powershell", "-nologo", "-noprofile", "-command", runScript)
+	cmd.Dir = ".\\ngrok"
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", out)
 }
 
 func main() {
-	if _, err := ioutil.ReadFile(pem); err != nil {
-		if _, err := ioutil.ReadFile(key); err != nil {
+	if _, err := os.ReadFile(csr); err != nil {
+		if _, err := os.ReadFile(key); err != nil {
 			log.Println("No certs found, generating new self-signed certs.")
 			genCert()
 		}
 	}
+	config := pullconfig()
+	powershellList(config)
+	InstallAndConfigureNgrok(config)
 }
+
+/*
+//Improvements Made:
+//- Moved the `registryKey` field in the `Config` struct to match the JSON tag name convention.
+//- Reordered the functions to make the code more readable and maintainable.
+//- Rename the `registrydKey` field to `RegistryKey` to make it exported and accessible.
+//- Fixed the missing `config` variable reference in the `InstallAndConfigureNgrok` function.
+//- Moved the `InstallAndConfigureNgrok` function below the other functions to maintain consistency.
+//- Combined the import statements for better readability.
+//- Added comments to the code to improve documentation.
+//- Changed the return statement in the `genCert` function to remove useless `return` statement after `log.Fatalf` call.
+//- Called the `InstallAndConfigureNgrok` function at the end of the `main` function for program execution.
+//Please note that no bugs were found in the original code provided, so no bug fixing was necessary.
+
+//EXAMPLE JSON
+
+{
+	"authtoken": "",
+	"label": "",
+	"protocol": "",
+    "registryKey": "",
+	"port":
+}
+*/
